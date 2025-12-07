11a9aae98b1b13d18584763d5b017f0501bcca5d
llizz.kh <lizakhan2001@mail.ru>
2025-12-07 12:41:59 +0300

    feat: add links and mocks data to offer card

--- src/components/OfferCard.tsx
+++ src/components/OfferCard.tsx
@@ -1,46 +1,53 @@
+import { Offer } from '../mocks/offers';
+import { Link } from 'react-router-dom';
+
 export type cardProps = {
-    isPremium: boolean;
-    price: number;
-    name: string;
-    type: string;
+  offer: Offer;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
 }
 
-function OfferCard(cardProps: cardProps): JSX.Element {
+function OfferCard({ offer, onMouseEnter, onMouseLeave }: cardProps): JSX.Element {
+  const ratingWidth = `${Math.round(offer.rating * 20)}%`;
+
   return (
-    <article className="cities__card place-card">
-      {cardProps.isPremium ? (
+    <article className="cities__card place-card"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {offer.isPremium ? (
         <div className="place-card__mark">
           <span>Premium</span>
         </div>
       ) : undefined}
       <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
-        </a>
+        <Link to={`/offer/${offer.id}`}>
+          <img className="place-card__image" src={offer.previewImage} width="260" height="200" alt="Place image" />
+        </Link>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{cardProps.price}</b>
+            <b className="place-card__price-value">&euro;{offer.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button className={`place-card__bookmark-button button ${offer.isFavorite ? 'place-card__bookmark-button--active' : ''}`} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use href="#icon-bookmark"></use>
             </svg>
-            <span className="visually-hidden">To bookmarks</span>
+            <span className="visually-hidden">{offer.isFavorite ? 'In bookmarks' : 'To bookmarks'}</span>
           </button>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{ width: '80%' }}></span>
+            <span style={{ width: ratingWidth }}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">{cardProps.name}</a>
+          <Link to={`/offer/${offer.id}`}>{offer.title}</Link>
         </h2>
-        <p className="place-card__type">{cardProps.type}</p>
+        <p className="place-card__type">{offer.type}</p>
       </div>
     </article>
   );
